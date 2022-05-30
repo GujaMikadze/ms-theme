36143c6825e575120a3a35a5294b3b58d308e79c
GujaMikadze <mikadzeguja@gmail.com>
2022-05-30 18:10:54 +0400

    user meta update when video is finished

--- functions/inc/ajaxcalls.php
+++ functions/inc/ajaxcalls.php
@@ -5,5 +5,11 @@ add_action('wp_ajax_nopriv_ meta_update', 'meta_update');
 
 
 function meta_update() {
-   update_user_meta($_POST['user'],$_POST['post'],['course_id' => $_POST['post'], 'current_section' => $_POST['section'], 'current_lession' => $_POST['video']]);
+   $user_data = get_user_meta($_POST['user'], 'course_enroll');
+   $key = array_search($_POST['post'], array_column($user_data, 'course_id'));
+   $watched = $user_data[$key]['watched_lesson'];
+   if(!array_search($_POST['video'], $watched) && array_search($_POST['video'], $watched) !== 0) {
+      $watched[] = $_POST['video'];
+   }
+   update_user_meta($_POST['user'],'course_enroll',['course_id' => $_POST['post'], 'current_section' => $_POST['section'], 'current_lesson' => $_POST['nextvid'], 'watched_lesson' => $watched], $user_data[$key]);
 }
